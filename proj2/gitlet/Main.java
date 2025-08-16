5472a93dc316f4ee5f67cf27ae3418e2b9543b03
bepsik777 <potocki.alex@gmail.com>
2025-08-16 15:03:57 +0200

    Implement status command

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -39,7 +39,7 @@ public class Main {
                 break;
             case "commit":
                 if (args.length == 1) {
-                    System.out.println("pls provide a commit message");
+                    System.out.println("Please enter a commit message.");
                     break;
                 }
                 String message = args[1];
@@ -85,6 +85,9 @@ public class Main {
                 break;
             case "print-index":
                 printStagingArea();
+                break;
+            case "status":
+                Repository.status();
                 break;
         }
     }
